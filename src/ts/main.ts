78a4498ae6d2fb0c0169fd95f887595643f9fba0
Theoreb <108896274+Theoreb@users.noreply.github.com>
2024-02-03 09:29:43 +0100

    First commit

---
+++ src/ts/main.ts
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const messageElement = document.getElementById("message");
+    if (messageElement) {
+        messageElement.innerText = "Hello from TypeScript!";
+    }
+});
